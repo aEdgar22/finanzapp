0928f8616a6d8a757aa62851a191ddaa0f78f6ad
edgar <a.edgar2209@gmail.com>
2022-10-29 20:09:57 -0500

    add/creacion hook useShowPassword

--- src/pages/auth/hooks/useShowPassword.tsx
+++ src/pages/auth/hooks/useShowPassword.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useState } from "react";
 
 function useShowPassword(state: boolean) {
-  const [show, setShow] = useState(state);
+  const [show, setShow] = useState<boolean>(state);
 
   const handleClick = (): void => {
     setShow(!show);
